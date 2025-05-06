ac8fe95433e0a9bdd558fd37d109707cf467597e
logeshmokkamayan <logeshmokkamayan@gmail.com>
2025-05-06 15:25:28 +0530

    Add files via upload

---
+++ if .c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+   printf("Enter the value: \n");
+   scanf("%d",&a);
+   if(a>10)
+        printf("The number is greater than 10. \n");
+        
+   return 0;
+   
+}
